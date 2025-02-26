7d097447c5d21db5511c37dfdff79f0a9b8e3004
ZorAnderius <gabriel.zorander@gmail.com>
2025-02-26 16:43:13 +0000

    add: morgan config and change response status

--- src/controllers/contactsControllers.js
+++ src/controllers/contactsControllers.js
@@ -17,7 +17,7 @@ export const getOneContact = async (req, res) => {
   if (!contact) throw HttpError(404, "Not found");
   res.json({
     status: 200,
-    message: `Successfully found contact with ${id}`,
+    message: `Successfully found contact with ID: ${id}`,
     data: contact,
   });
 };
@@ -26,16 +26,16 @@ export const deleteContact = async (req, res) => {
   const { id } = req.params;
   const contact = await contactsService.removeContact(id);
   if (!contact) throw HttpError(404, "Not found");
-  res.status(200).json({
+  res.json({
     status: 200,
-    message: `Contact with ${id} delete successfully`,
+    message: `Contact with ID: ${id} was deleted successfully`,
     data: contact,
   });
 };
 
 export const createContact = async (req, res) => {
   const contact = await contactsService.addContact(req.body);
-  res.json({
+  res.status(201).json({
     status: 201,
     message: "Contact was created successfully",
     data: contact,
@@ -48,7 +48,7 @@ export const updateContact = async (req, res) => {
   const { id } = req.params;
   const contact = await contactsService.updateContact(id, req.body);
   if (!contact) throw HttpError(404, "Not found");
-  res.status(200).json({
+  res.json({
     status: 200,
     message: `Contact with ${id} was successfully updated`,
     data: contact,
