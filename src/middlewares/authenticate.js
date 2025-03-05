dc704e879753c40bfa3a4b38b90336f6c7dec323
ZorAnderius <gabriel.zorander@gmail.com>
2025-03-05 09:44:50 +0000

    update: error text message when token or user is missing

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -5,16 +5,16 @@ import { verifyToken } from "../helpers/jwt.js";
 const authenticate = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization)
-    return next(HttpError(401, "Authorization header missing"));
+    return next(HttpError(401, "Not authorized"));
   
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") return next(HttpError(401, "Bearer is missing"));
+  if (bearer !== "Bearer") return next(HttpError(401, "Not authorized"));
 
   const { data, error } = verifyToken(token);
   if (error) return next(HttpError(401, error.message));
 
   const user = await findUser({ email: data.email });
-  if (!user) return next(HttpError(401, "User not found"));
+  if (!user) return next(HttpError(401, "Not authorized"));
   req.owner_id = user.id;
   next();
 };
